d028594f4c6fcee77af8518be78033990e610a92
TotoroSan <philipp.gsell@live.de>
2024-01-19 20:19:29 +0100

    Testing GitHub Eclipse Integration

--- CrunchifyRESTJerseyExample/src/main/java/com/crunchify/restjersey/GitTest.java
+++ CrunchifyRESTJerseyExample/src/main/java/com/crunchify/restjersey/GitTest.java
@@ -2,6 +2,6 @@ package com.crunchify.restjersey;
 
 public class GitTest {
   public static void main(String[] args) {
-    System.out.println("Git is das");
+    System.out.println("Git is xxxxx");
   }
 }
